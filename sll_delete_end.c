f4a2583991884ba9a2212a2bce31518309d665ef
sharon22wh1a1232 <121919579+sharon22wh1a1232@users.noreply.github.com>
2023-10-13 11:26:52 +0530

    Add files via upload

---
+++ sll_delete_end.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+        int data;
+        struct node*link;
+};
+void delete(struct node*head);
+int main()
+{
+        struct node*head=(struct node*)malloc(sizeof(struct node));
+        head->data=2000;
+        head->link=NULL;
+        struct node*current=(struct node*)malloc(sizeof(struct node));
+        current->data=3000;
+        current->link=NULL;
+        head->link=current;
+        delete(head);
+}
+void delete(struct node*head)
+{
+	struct node*temp=head;
+	temp=temp->link;
+	free(temp);
+	temp=NULL;
+	printf("%d",head->data);
+}
+
+
